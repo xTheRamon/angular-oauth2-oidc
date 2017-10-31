97e0c8700c9ec840bd27f76f2279dee2dac08f5f
Ramon Swilem <ramon@ionline.nl>
2017-10-31 15:20:11 +0100

    Fix

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+import { ModuleWithProviders } from '@angular/core';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/first';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/publish';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/race';
+export * from './oauth-service';
+export * from './token-validation/jwks-validation-handler';
+export * from './token-validation/null-validation-handler';
+export * from './token-validation/validation-handler';
+export * from './url-helper.service';
+export * from './auth.config';
+export * from './types';
+export * from './tokens';
+export declare class OAuthModule {
+    static forRoot(): ModuleWithProviders;
+}
---
+++ types.d.ts
@@ -0,0 +1,71 @@
+/**
+ * Additional options that can be passt to tryLogin.
+ */
+export declare class LoginOptions {
+    /**
+     * Is called, after a token has been received and
+     * successfully validated.
+     *
+     * Deprecated:  Use property ``events`` on OAuthService instead.
+     */
+    onTokenReceived?: (receivedTokens: ReceivedTokens) => void;
+    /**
+     * Hook, to validate the received tokens.
+     * Deprecated:  Use property ``tokenValidationHandler`` on OAuthService instead.
+     */
+    validationHandler?: (receivedTokens: ReceivedTokens) => Promise<any>;
+    /**
+     * Called when tryLogin detects that the auth server
+     * included an error message into the hash fragment.
+     *
+     * Deprecated:  Use property ``events`` on OAuthService instead.
+     */
+    onLoginError?: (params: object) => void;
+    /**
+     * A custom hash fragment to be used instead of the
+     * actual one. This is used for silent refreshes, to
+     * pass the iframes hash fragment to this method.
+    */
+    customHashFragment?: string;
+    /**
+     * Set this to true to disable the oauth2 state
+     * check which is a best practice to avoid
+     * security attacks.
+     * As OIDC defines a nonce check that includes
+     * this, this can be set to true when only doing
+     * OIDC.
+     */
+    disableOAuth2StateCheck?: boolean;
+}
+/**
+ * Defines a simple storage that can be used for
+ * storing the tokens at client side.
+ * Is compatible to localStorage and sessionStorage,
+ * but you can also create your own implementations.
+ */
+export declare abstract class OAuthStorage {
+    abstract getItem(key: string): string | null;
+    abstract removeItem(key: string): void;
+    abstract setItem(key: string, data: string): void;
+}
+/**
+ * Represents the received tokens, the received state
+ * and the parsed claims from the id-token.
+ */
+export declare class ReceivedTokens {
+    idToken: string;
+    accessToken: string;
+    idClaims?: object;
+    state?: string;
+}
+/**
+ * Represents the parsed and validated id_token.
+ */
+export interface ParsedIdToken {
+    idToken: string;
+    idTokenClaims: object;
+    idTokenHeader: object;
+    idTokenClaimsJson: string;
+    idTokenHeaderJson: string;
+    idTokenExpiresAt: number;
+}
